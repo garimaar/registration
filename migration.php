8bcb8b800f002439896063e8fb15962579fee20a
garima <garimaarora121999@gmail.com>
2021-07-13 12:55:23 +0530

    second commit

---
+++ migration.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $myfile = fopen("migration.txt", "r") or die("Unable to open file!");
+    echo fread($myfile, filesize("migration.txt"));
+    fclose($myfile);
+
+    ?>
+
+</body>
+
+</html>

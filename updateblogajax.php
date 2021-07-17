6873a019e09516441dd634b2be14e4e3acf73768
garima <garimaarora121999@gmail.com>
2021-07-17 19:17:19 +0530

    blog listing update

--- updateblogajax.php
+++ updateblogajax.php
@@ -8,8 +8,7 @@ if (isset($_REQUEST['id'])) {
     $title = $_POST['title'];
     $content = $_POST['content'];
 
-    $query = "UPDATE blog " . "SET title = $title content= $content" .
-        "WHERE id = $id";
+    $query = "UPDATE blog SET title='$title' content='$content' WHERE id='$id'";
     $data = mysqli_query($con, $query);
 
     if ($data) {
